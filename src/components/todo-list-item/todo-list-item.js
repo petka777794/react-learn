e36480cd4193dfc02c5991a485d46973d2f56817
dmitry_kalodzka <dmitry.kalodzka@certaintysoftware.com>
2019-03-22 19:28:39 +0300

    no message

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component {
+
+  // constructor() {
+  //   super();
+
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`);
+  //   };
+    // this.state = {
+    //   done: false,
+    // };
+  // }
+
+  state = {
+    done: false,
+  };
+
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
+  };
+
+  //так привязывается функция с this к объекту а не к прототипу
+  //но это еще не стандарт языка
+  //стандарт это то, что в комментариях. добавлять функцию в конструкторе
+
+  render() {
+    const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+          className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  };
+}
