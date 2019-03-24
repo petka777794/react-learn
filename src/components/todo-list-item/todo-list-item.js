53cc242e57375958f420ab32a9504dbcfe1e080c
dmitry_kalodzka <dmitry.kalodzka@certaintysoftware.com>
2019-03-24 12:14:42 +0300

    no message

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
-
   // constructor() {
   //   super();
 
   //   this.onLabelClick = () => {
   //     console.log(`Done: ${this.props.label}`);
   //   };
-    // this.state = {
-    //   done: false,
-    // };
+  // this.state = {
+  //   done: false,
+  // };
   // }
-
-  state = {
-    done: false,
-  };
-
-  onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
-  };
 
   //так привязывается функция с this к объекту а не к прототипу
   //но это еще не стандарт языка
   //стандарт это то, что в комментариях. добавлять функцию в конструкторе
 
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label, 
+            onDeleted,
+            onToggleImportant, 
+            onToggleDone,
+            done,
+            important,
+          } = this.props;
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}>
+          onClick={onToggleDone}
+        >
           {label}
         </span>
 
-        <button type="button"
-          className="btn btn-outline-success btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={onToggleImportant}
+        >
           <i className="fa fa-exclamation" />
         </button>
 
-        <button type="button"
-          className="btn btn-outline-danger btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
+        >
           <i className="fa fa-trash-o" />
         </button>
       </span>
     );
-  };
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
+import '../src/fonts/font-awesome.min.css';
+import '../src/styles/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
